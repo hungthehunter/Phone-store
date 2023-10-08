8d4bb814db41d54a5d6302945cf1fc957a294f0a
hungthehunter <133243197+hungthehunter@users.noreply.github.com>
2023-10-08 12:28:41 +0700

    Initial Commit.

---
+++ phone/src/view/Login.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class Login extends JFrame {
+
+	public Login(){
+	this.init();	
+	}
+	
+	public void init() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400,500);
+		this.setLocationRelativeTo(null);
+		
+		
+		
+		
+
+	}
+
+}
